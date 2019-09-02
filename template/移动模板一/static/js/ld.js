cf91805987c47a7196ccdf598cd2245ff39cc7ad
rungit1996 <rungit@163.com>
2019-09-02 14:48:46 +0800

    完善移动模板一

--- template/移动模板一/static/js/ld.js
+++ template/移动模板一/static/js/ld.js
@@ -1,6 +1,6 @@
 
 const ld = [
-{nickname:"小明",qq:"",wx:"xiaoming",tel:"18888888888"}
+{nickname:"小明",qq:"",wx:"xiaoming",tel:"18888888888",gender:"他"}
 ]
 
 const ld_index = Math.floor(Math.random()*ld.length);
@@ -15,7 +15,11 @@ for (let i = 0; i < wxs.length; i++) {
     wxs[i].innerHTML = ld[ld_index].wx;
     wxs[i].style.color="red";
 }
+const genders = document.getElementsByClassName("gender");
+for (let i = 0; i < genders.length; i++) {
+    genders[i].innerHTML = ld[ld_index].gender;
+}
 
 function copyText() {
-    // 事件代码
+    // 复制事件代码
 };
